0c3874874baaabc8d6acea311abc35b29fc7be30
studentpriya <162472783+studentpriya@users.noreply.github.com>
2024-03-06 20:59:44 +0530

    Add files via upload
    Program Addition of two integer nos.

---
+++ Addition.java
@@ -0,0 +1,15 @@
+class Addition{
+public static void main(String args[]){
+int a=10;
+int b=20;
+int c=a+b;
+
+System.out.println(c);
+
+System.out.println("sum="+c);
+System.out.println("a="+a+"\tb="+b+"\nc="+c);
+System.out.println("a="+a);
+System.out.println("b="+b);
+System.out.println("c="+c);
+}
+}
